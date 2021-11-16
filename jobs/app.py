2ea213ff4fc9ac87d4f93f4c6b55d0025aa48e3f
Lucas Beeman <lucasbeeman12@gmail.com>
2021-11-16 10:19:46 -0500

    made some changes

--- jobs/app.py
+++ jobs/app.py
@@ -1,11 +1,8 @@
-from os import close
-from sqlite3.dbapi2 import Connection, connect
-from flask import Flask, request
-from flask import render_template, g
 import sqlite3
 import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 
-PATH = "db/jobs.sqlite"
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
@@ -16,42 +13,52 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values = (), commit = False, single = False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-        cursor.close()
+
+    cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
-    getattr(g, '_connection', None)
-    if open_connection == None:
-        close()
-    return open_connection()
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
-
-@app.route("/")
-@app.route("/jobs")
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    return render_template('index.html', jobs = jobs)
+    return render_template('index.html', jobs=jobs)
 
-@app.route("/job/<job_id>")
+@app.route('/job/<job_id>')
 def job(job_id):
-    render_template()
-    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',([job_id]), single=True)
-    return render_template("job.html", job = job)
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
 
-@app.route("/employer/<employer_id>")
+@app.route('/employer/<employer_id>')
 def employer(employer_id):
-    employer = execute_sql('SELECT * FROM employer WHERE id=?', ([employer_id]), single=True)
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', ([employer_id]))
-    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', ([employer_id]))
-    return render_template("employer.html", employer = employer, jobs = jobs, reviews = reviews)
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
-    return render_template('review.html', employer_id=employer_id)
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id) 
